47ccc176968bc2c864da6a666feca5c1a7f3acb5
DesmondWillowbrook <kartavya0703@gmail.com>
2020-11-04 11:24:17 +0530

    Reformatted

--- src/lib.rs
+++ src/lib.rs
@@ -484,37 +484,27 @@ pub mod plot_comp {
             .axis_desc_style(("sans-serif", 15))
             .draw()?;
 
-/*mismatched types
-
-expected a tuple with 2 elements, found one with 3 elements
-
-note: expected tuple `(&'static plotters::style::RGBColor, &'static str)`
-         found tuple `(&'static plotters::style::RGBColor, &'static str, [closure@src\lib.rs:489:46: 489:60])` */
-        const COLOR_ARR: [(&RGBColor, &str); 5] = [
+        //Draw bases
+        for i in [
             (&RGBColor(255, 0, 0), "Base A"),
             (&RGBColor(0, 255, 0), "Base T"),
             (&RGBColor(0, 0, 255), "Base G"),
             (&RGBColor(255, 255, 0), "Base C"),
             (&RGBColor(0, 255, 255), "Unknown Base"),
-        ];
-
-        let read_arr = [
+        ].iter().zip([
             |r: &Read| r.A,
             |r: &Read| r.T,
             |r: &Read| r.G,
             |r: &Read| r.C,
             |r: &Read| r.N
-        ];
-        
-        //Draw bases
-        for i in (0..COLOR_ARR.len()).zip(0..read_arr.len()) {
+        ].iter()) {
             chart
                 .draw_series(LineSeries::new(
-                    comp.iter().map(|r| (r.pos as u32, read_arr[i.1](r))),
-                    COLOR_ARR[i.0].0,
+                    comp.iter().map(|r| (r.pos as u32, i.1(r))),
+                    i.0.0,
                 ))?
-                .label(COLOR_ARR[i.0].1)
-                .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], COLOR_ARR[i.0].0));
+                .label(i.0.1)
+                .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], i.0.0));
         }
 
     /*    chart
