8836321eb630584ee1a14a25acc4f7e88fb0519a
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-25 23:53:41 +0530

    Corrected file distribution

--- data/download-extract/process_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script takes in n-number of fastq files and samples them, ready for plotting
-
-for arg; do
-  echo $arg
-  cargo run --bin extract-fastq-comp 10000 $arg $arg"comp.json" 
-done
