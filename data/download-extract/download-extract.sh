72c01a6641f6aa16d50446a6a36013048297f209
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-18 13:43:44 +0530

    Add python script for downloading files.

--- data/download-extract/download-extract.sh
+++ data/download-extract/download-extract.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Should be run from project root.
+# Test command: cat data/results_example_gds.txt | bash -x data/download-extract/download-extract.sh
+
 # Made with reference to the following format:
 # 1	Mus musculus	RNA-Seq	SRR12478073	ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/073/SRR12478073/SRR12478073.fastq.gz	Transcriptome analysis of the TA muscles from WT and Dmd Exon 51 Knockout mice
 # 2	Mus musculus	RNA-Seq	SRR12926516	ftp.sra.ebi.ac.uk/vol1/fastq/SRR129/016/SRR12926516/SRR12926516_1.fastq.gz	Prenatal exposure to environmental toxins induces sexually dimorphic transcriptome changes in neonatal offspring
@@ -22,9 +26,6 @@ while IFS='$\n' read -r line; do
     # $'\"' is escaped double quote ("). God, why does this need to be explaned? Because bash.
     echo {metadata: {serial_num:$'\"'$serial_num$'\"', species:$'\"'$species$'\"', lib_type:$'\"'$lib_type$'\"', srr_number:$'\"'$srr_number$'\"', title:$'\"'$title$'\"'}, data:.} > $filter_file
 
-    wget -O $srr_number".fastq.gz" $ftp_url;
-    gunzip $srr_number".fastq.gz";
-
-    ./target/release/extract_comp --stdout 100000 "in.fastq" | \
+    python3 ./data/download-extract/sample_srr.py $srr_number 0 10000000 | ./target/release/extract_comp --stdin --stdout 100000 | \
     jq -f $filter_file > $srr_number"_comp.json"
 done
