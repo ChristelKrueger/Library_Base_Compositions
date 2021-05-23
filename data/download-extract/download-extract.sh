d3920fdf610b0c5c63b573cb15abb2cf5533dcba
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-05-23 17:53:45 +0530

    Extract srr_number For naming output file. Fix to previous commit.

--- data/download-extract/download-extract.sh
+++ data/download-extract/download-extract.sh
@@ -13,5 +13,6 @@
 set -euo pipefail
 
 while IFS='$\n' read -r line; do
-    echo -e "$line\t$(python3 ./data/download-extract/sample_srr.py $srr_number 2 1000 | ./target/release/extract_comp --stdin --stdout --tsv --trim 50 1000)" >> ./data/download-extract/output.csv
+    srr_number=`echo "$line" | awk -F '\t' '{ print $4 }'`
+    echo -e "$line\t$(python3 ./data/download-extract/sample_srr.py $srr_number 2 100 | ./target/release/extract_comp --stdin --stdout --tsv --trim 50 100)" >> ./data/download-extract/output.tsv
 done
